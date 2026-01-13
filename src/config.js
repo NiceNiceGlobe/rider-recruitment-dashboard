6901363f3635f2ce5762e04b14105f2d969ec81f
neothabethe <neothabethe2004@gmail.com>
2026-01-14 01:39:39 +0200

    Update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const isDev = import.meta.env.DEV;
 
 const devBase = "https://localhost:7156";
-const prodBase = "https://nicenice-server-yo96.onrender.com";
+const prodBase = "https://valternative-server.onrender.com";
 
 export const API_BASE = isDev ? devBase : prodBase;
